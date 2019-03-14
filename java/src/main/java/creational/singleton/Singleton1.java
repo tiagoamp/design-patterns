6063fe82e79dd92ca4d386ba8943dc26db3d5fb2
tiago.albuquerque <tiagoamp@>
2019-03-14 14:48:18 -0300

    Adjust class packages

--- java/src/main/java/creational/singleton/Singleton1.java
+++ java/src/main/java/creational/singleton/Singleton1.java
@@ -1,4 +1,4 @@
-package creational;
+package creational.singleton;
 
 /**
  * Classic Singleton implementation
--- java/src/main/java/creational/singleton/Singleton2.java
+++ java/src/main/java/creational/singleton/Singleton2.java
@@ -1,4 +1,4 @@
-package creational;
+package creational.singleton;
 
 /**
  * Better implementation
