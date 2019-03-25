ebb5b946d9f026b633c883d7908df2bc2035d674
tiago.albuquerque <tiagoamp@>
2019-03-25 16:01:35 -0300

    Implement first version os Strategy Pattern

---
+++ java/src/main/java/behavioral/strategy/IAttack.java
@@ -0,0 +1,7 @@
+package behavioral.strategy;
+
+public interface IAttack {
+
+	String execute();
+	
+}
---
+++ java/src/main/java/behavioral/strategy/Shoryuken.java
@@ -0,0 +1,10 @@
+package behavioral.strategy;
+
+public class Shoryuken implements IAttack {
+
+	@Override
+	public String execute() {
+		return "shoryuuuuuuukeeeennnnnnn!!!!";
+	}
+
+}
